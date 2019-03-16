2a2003ef230271008fdef22f54c0143ab0c71e90
1301702486 <1301702486@qq.com>
2019-03-16 15:15:13 +0800

    Login interface

---
+++ PgsqlAccess.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+public class PgsqlAccess
+{
+    private static string Host = "localhost";
+    private static string User = "postgres";
+    private static string DBname = "whu";
+    private static string Password = "postgres";
+    private static string Port = "5432";
+
+    public static NpgsqlConnection GetConnection()
+    {
+        string connStr = 
+            String.Format(
+                    "Server={0}; User Id={1}; Database={2}; Port={3}; Password={4};",
+                    Host,
+                    User,
+                    DBname,
+                    Port,
+                    Password);
+        var conn = new NpgsqlConnection(connStr);
+        conn.Open();
+        return conn;
+    }
+
+    public static void InsertInto(string tableName, string colName, string value)
+    {
+        using(var conn = GetConnection())
+        {
+            string insertStr = 
+                String.Format(
+                    "INSERT INTO {0} {1} VALUES {2};",
+                    tableName,
+                    colName,
+                    value);
+            using (var cmd = new NpgsqlCommand(insertStr, conn))
+            {
+                cmd.Prepare();
+                
+            }
+        }
+    }
+}
--- pgManager/Form1.cs
+++ pgManager/Form1.cs
@@ -7,14 +7,116 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace pgManager
 {
     public partial class Form1 : Form
     {
+        private static string Host = "localhost";
+        private static string User = "postgres";
+        private static string DBname = "whu";
+        private static string Password = "postgres";
+        private static string Port = "5432";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public static NpgsqlConnection GetConnection()
+        {
+            string connStr = string.Format(
+                        "Server={0}; User Id={1}; Database={2}; Port={3}; Password={4};",
+                        Host,
+                        User,
+                        DBname,
+                        Port,
+                        Password);
+            var conn = new NpgsqlConnection(connStr);
+            conn.Open();
+            return conn;
+        }
+
+        public static void WhereClause()
+        {
+            using (var conn = GetConnection())
+            {
+                string whereStr = string.Format(
+                    "where;"
+                    );
+            }
+        }
+
+        // 简易版insert（插入一个单值）
+        public static void Insert(string tableName, string colName, string value)
+        {
+            using (var conn = GetConnection())
+            {
+                string insertStr = string.Format(
+                        "insert into {0} {1} values @value;",
+                        tableName,
+                        colName);
+                using (var cmd = new NpgsqlCommand(insertStr, conn))
+                {
+                    cmd.Prepare();
+                    cmd.Parameters.AddWithValue("@value", value);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // 简易版update（把colName列的所有值改为value）
+        public static void Update(string tableName, string colName, string value)
+        {
+            using (var conn = GetConnection())
+            {
+                string updateStr = string.Format(
+                        "update {0} set {1} = @value;",
+                        tableName,
+                        colName);
+                using (var cmd = new NpgsqlCommand(updateStr, conn))
+                {
+                    cmd.Prepare();
+                    cmd.Parameters.AddWithValue("@value", value);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        // 简易版delete（删除所有colName=value的记录）
+        public static void Delete(string tableName, string colName, string value)
+        {
+            using (var conn = GetConnection())
+            {
+                string deleteStr = string.Format(
+                    "delete from {0} where {1} = {2};",
+                    tableName,
+                    colName,
+                    value);
+                using (var cmd = new NpgsqlCommand(deleteStr, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            Host = textBoxHost.Text;
+            User = textBoxUserName.Text;
+            DBname = textBoxDBName.Text;
+            Password = textBoxPswd.Text;
+            Port = textBoxPort.Text;
+            var conn = GetConnection();
+            label2.Text = "登录成功！";
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            timer1.Stop();
         }
     }
 }
