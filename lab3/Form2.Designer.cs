ef37eb99129394117c66a0f851c21195923ca3ee
JudyHU <992617751@qq.com>
2019-03-19 22:20:09 +0800

    still has some problems

--- lab3/Form2.Designer.cs
+++ lab3/Form2.Designer.cs
@@ -30,15 +30,17 @@
         {
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // treeView1
             // 
             this.treeView1.Font = new System.Drawing.Font("Tahoma", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.treeView1.Location = new System.Drawing.Point(21, 52);
+            this.treeView1.Location = new System.Drawing.Point(24, 38);
+            this.treeView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(350, 492);
+            this.treeView1.Size = new System.Drawing.Size(375, 593);
             this.treeView1.TabIndex = 0;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
@@ -46,23 +48,35 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(409, 52);
+            this.dataGridView1.Location = new System.Drawing.Point(436, 38);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 30;
-            this.dataGridView1.Size = new System.Drawing.Size(696, 492);
+            this.dataGridView1.Size = new System.Drawing.Size(762, 593);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(663, 235);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 34);
+            this.textBox1.TabIndex = 2;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1136, 617);
+            this.ClientSize = new System.Drawing.Size(1234, 644);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.treeView1);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei UI", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.Text = "Postgres";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +85,6 @@
 
         private System.Windows.Forms.TreeView treeView1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- lab3/Form2.cs
+++ lab3/Form2.cs
@@ -15,6 +15,8 @@ namespace lab3
 {
     public partial class Form2 : Form
     {
+        string name;//节点名称
+
         // 获取PgsqlAccess单例
         PgsqlAccess pg = PgsqlAccess.getInstance();
 
@@ -33,10 +35,26 @@ namespace lab3
         }
         private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
         {
-            
-            if (e.Node.Bounds.Contains(e.Location))
+            name = e.Node.Text.ToString();
+
+            if (name=="course")
             {
-                //表的内容显示
+                DataSet ds = new DataSet();
+                NpgsqlCommand comm = new NpgsqlCommand();
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter();
+                //数据库连接字符串，自行更改数据库路径和名字
+                using (connection=pg.GetConnection())
+                comm.Connection = connection;
+                comm.CommandType = CommandType.Text;
+                comm.CommandText = "select * from course";//查询student表
+                da.SelectCommand = comm;
+                da.Fill(ds);
+                textBox1.Text = ds.Tables[0].Rows[0][0].ToString();//显示第一行第一列的一个值
+                this.dataGridView1.DataSource = ds.Tables[0].DefaultView;//表从起始行显示在dataGridView里
+
+               
+
+                
             }
         }
         //add nodes towards the treeview
@@ -46,7 +64,7 @@ namespace lab3
             TreeNode tNode = treeView1.Nodes.Add("whu");
             //connect to the database
             string sql = "SELECT tablename FROM pg_tables WHERE tablename NOT LIKE 'pg%' AND tablename NOT LIKE 'sql_%' ORDER BY tablename;";
-            using (connection = pg.GetConnection())
+            using (connection = pg.GetConnection())//用了using所以不需要关闭了
             {
                 using (var dataAdapter = new NpgsqlDataAdapter(sql, connection))
                 {
