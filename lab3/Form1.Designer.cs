2f22a019a9c538befd4b8e272ca8a91bd15b0b51
JudyHU <992617751@qq.com>
2019-03-19 21:05:57 +0800

    a little change

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -46,20 +46,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(136, 236);
+            this.label2.Location = new System.Drawing.Point(153, 283);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 23);
+            this.label2.Size = new System.Drawing.Size(123, 26);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password:";
             // 
             // textBoxPswd
             // 
-            this.textBoxPswd.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxPswd.Location = new System.Drawing.Point(272, 236);
+            this.textBoxPswd.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPswd.Location = new System.Drawing.Point(306, 283);
             this.textBoxPswd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxPswd.Name = "textBoxPswd";
             this.textBoxPswd.PasswordChar = '*';
-            this.textBoxPswd.Size = new System.Drawing.Size(247, 25);
+            this.textBoxPswd.Size = new System.Drawing.Size(277, 39);
             this.textBoxPswd.TabIndex = 5;
             this.textBoxPswd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPswd_KeyPress);
             // 
@@ -69,10 +69,10 @@
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(406, 317);
+            this.button1.Location = new System.Drawing.Point(457, 380);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(92, 43);
+            this.button1.Size = new System.Drawing.Size(104, 52);
             this.button1.TabIndex = 6;
             this.button1.Text = "Okay";
             this.button1.UseVisualStyleBackColor = false;
@@ -81,10 +81,10 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(551, 317);
+            this.button2.Location = new System.Drawing.Point(620, 380);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(92, 43);
+            this.button2.Size = new System.Drawing.Size(104, 52);
             this.button2.TabIndex = 7;
             this.button2.Text = "Cancle";
             this.button2.UseVisualStyleBackColor = true;
@@ -94,19 +94,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(184, 81);
+            this.label1.Location = new System.Drawing.Point(207, 97);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 23);
+            this.label1.Size = new System.Drawing.Size(72, 26);
             this.label1.TabIndex = 5;
             this.label1.Text = "Host:";
             // 
             // textBoxHost
             // 
-            this.textBoxHost.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxHost.Location = new System.Drawing.Point(272, 79);
+            this.textBoxHost.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxHost.Location = new System.Drawing.Point(306, 95);
             this.textBoxHost.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxHost.Name = "textBoxHost";
-            this.textBoxHost.Size = new System.Drawing.Size(247, 25);
+            this.textBoxHost.Size = new System.Drawing.Size(277, 39);
             this.textBoxHost.TabIndex = 1;
             this.textBoxHost.Text = "localhost";
             // 
@@ -114,19 +114,19 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(125, 158);
+            this.label3.Location = new System.Drawing.Point(141, 190);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(121, 23);
+            this.label3.Size = new System.Drawing.Size(140, 26);
             this.label3.TabIndex = 7;
             this.label3.Text = "User name:";
             // 
             // textBoxUserName
             // 
-            this.textBoxUserName.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxUserName.Location = new System.Drawing.Point(272, 156);
+            this.textBoxUserName.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUserName.Location = new System.Drawing.Point(306, 187);
             this.textBoxUserName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxUserName.Name = "textBoxUserName";
-            this.textBoxUserName.Size = new System.Drawing.Size(247, 25);
+            this.textBoxUserName.Size = new System.Drawing.Size(277, 39);
             this.textBoxUserName.TabIndex = 3;
             this.textBoxUserName.Text = "postgres";
             // 
@@ -134,19 +134,19 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(139, 198);
+            this.label4.Location = new System.Drawing.Point(156, 238);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(107, 23);
+            this.label4.Size = new System.Drawing.Size(125, 26);
             this.label4.TabIndex = 9;
             this.label4.Text = "Database:";
             // 
             // textBoxDB
             // 
-            this.textBoxDB.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxDB.Location = new System.Drawing.Point(272, 196);
+            this.textBoxDB.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDB.Location = new System.Drawing.Point(306, 235);
             this.textBoxDB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxDB.Name = "textBoxDB";
-            this.textBoxDB.Size = new System.Drawing.Size(247, 25);
+            this.textBoxDB.Size = new System.Drawing.Size(277, 39);
             this.textBoxDB.TabIndex = 4;
             this.textBoxDB.Text = "whu";
             // 
@@ -154,27 +154,27 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(189, 119);
+            this.label5.Location = new System.Drawing.Point(213, 143);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(57, 23);
+            this.label5.Size = new System.Drawing.Size(65, 26);
             this.label5.TabIndex = 11;
             this.label5.Text = "Port:";
             // 
             // textBoxPort
             // 
-            this.textBoxPort.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxPort.Location = new System.Drawing.Point(272, 117);
+            this.textBoxPort.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPort.Location = new System.Drawing.Point(306, 140);
             this.textBoxPort.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxPort.Name = "textBoxPort";
-            this.textBoxPort.Size = new System.Drawing.Size(247, 25);
+            this.textBoxPort.Size = new System.Drawing.Size(277, 39);
             this.textBoxPort.TabIndex = 2;
             this.textBoxPort.Text = "5432";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(735, 407);
+            this.ClientSize = new System.Drawing.Size(827, 488);
             this.Controls.Add(this.textBoxPort);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBoxDB);
--- lab3/Form2.Designer.cs
+++ lab3/Form2.Designer.cs
@@ -29,25 +29,39 @@
         private void InitializeComponent()
         {
             this.treeView1 = new System.Windows.Forms.TreeView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Font = new System.Drawing.Font("Tahoma", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeView1.Location = new System.Drawing.Point(21, 52);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(350, 422);
+            this.treeView1.Size = new System.Drawing.Size(350, 492);
             this.treeView1.TabIndex = 0;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(409, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(696, 492);
+            this.dataGridView1.TabIndex = 1;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1136, 617);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.treeView1);
             this.Name = "Form2";
             this.Text = "Postgres";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +70,6 @@
         #endregion
 
         private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
